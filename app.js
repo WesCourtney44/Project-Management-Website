856b5a98b57e54bac59cdece0fa49d81d35257ec
WesCourtney44 <wescourtney44@gmail.com>
2016-09-13 06:24:53 -0400

    Added more comments. Signed-off-by: WesCourtney44 <wescourtney44@gmail.com>

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var StaffMember = (function () {
+    //Brings in info about the staff member
     function StaffMember(title, firstName, lastName, position) {
         this.url = "Images/Staff/" + firstName + " " + lastName + ".jpg";
         this.altText = "Image of " + title + " " + firstName + " " + lastName;
@@ -7,21 +8,29 @@ var StaffMember = (function () {
         this.lastName = lastName;
         this.position = position;
     }
+    //Generates a div containing an image,
+    //and info about the staff member.
     StaffMember.prototype.getDiv = function () {
+        //Creates div container and sets the
+        //tagName so it can be easily referenced in bootstrap/css
         var div = document.createElement('div');
         div.tagName = "StaffMember";
+        //creates image element and adds it to the container div
         var img = document.createElement('img');
         img.src = this.url;
         img.alt = this.altText;
         img.width = 112.5;
         img.height = 140;
         div.appendChild(img);
+        //creates name div and adds it to the container div
         var nameDiv = document.createElement('div');
         nameDiv.textContent = this.title + " " + this.firstName + " " + this.lastName;
         div.appendChild(nameDiv);
+        //creates position div and adds it to the container div
         var positionDiv = document.createElement('div');
         positionDiv.textContent = this.position;
         div.appendChild(positionDiv);
+        //returns the container
         return div;
     };
     return StaffMember;
@@ -29,14 +38,18 @@ var StaffMember = (function () {
 var Main = (function () {
     function Main() {
     }
+    //Main.main() is run when the body is finished loading
+    //I'm using it like a main method from java.
     Main.main = function () {
-        //Add staff members objects
+        //Add staff members objects to an array
+        //this method of adding users will need to be changed in future builds
         var staffMemberList = new Array();
         staffMemberList.push(new StaffMember("The Rev.", "Jack", "Cormack", "Head of Staff"));
         staffMemberList.push(new StaffMember("Pastor", "Andy", "Traister", "Commissioned Ruling Elder"));
         staffMemberList.push(new StaffMember("", "Susan", "Utz", "Music Director"));
         staffMemberList.push(new StaffMember("", "Rob", "Magallon", "Praise Music Leader"));
         staffMemberList.push(new StaffMember("", "Megan", "Broadus", "Wee Care Director"));
+        //generates html elements for each staff member and adds them to the document body
         var index = 0;
         while (index < staffMemberList.length) {
             document.body.appendChild(staffMemberList[index].getDiv());
--- app.ts
+++ app.ts
@@ -5,6 +5,8 @@
     firstName: string;
     lastName: string;
     position: string;
+
+    //Brings in info about the staff member
     constructor(title: string, firstName: string, lastName: string, position: string) {
         this.url = "Images/Staff/" + firstName + " " + lastName + ".jpg";
         this.altText = "Image of " + title + " " + firstName + " " + lastName;
@@ -13,10 +15,16 @@
         this.lastName = lastName;
         this.position = position;
     }
+
+    //Generates a div containing an image,
+    //and info about the staff member.
     public getDiv(): HTMLDivElement {
+        //Creates div container and sets the
+        //tagName so it can be easily referenced in bootstrap/css
         let div: HTMLDivElement = document.createElement('div');
         div.tagName = "StaffMember";
 
+        //creates image element and adds it to the container div
         let img: HTMLImageElement = document.createElement('img');
         img.src = this.url;
         img.alt = this.altText;
@@ -24,23 +32,28 @@
         img.height = 140;
         div.appendChild(img);
 
+        //creates name div and adds it to the container div
         let nameDiv: HTMLDivElement = document.createElement('div');
         nameDiv.textContent = this.title + " " + this.firstName + " " + this.lastName;
         div.appendChild(nameDiv);
 
+        //creates position div and adds it to the container div
         let positionDiv: HTMLDivElement = document.createElement('div');
         positionDiv.textContent = this.position;
         div.appendChild(positionDiv);
 
+        //returns the container
         return div;
     }
 }
 
 class Main {
-
+    //Main.main() is run when the body is finished loading
+    //I'm using it like a main method from java.
     public static main(): void {
 
-        //Add staff members objects
+        //Add staff members objects to an array
+        //this method of adding users will need to be changed in future builds
         let staffMemberList = new Array<StaffMember>();
         staffMemberList.push(new StaffMember("The Rev.", "Jack", "Cormack", "Head of Staff"));
         staffMemberList.push(new StaffMember("Pastor", "Andy", "Traister", "Commissioned Ruling Elder"));
@@ -48,6 +61,7 @@ class Main {
         staffMemberList.push(new StaffMember("", "Rob", "Magallon", "Praise Music Leader"));
         staffMemberList.push(new StaffMember("", "Megan", "Broadus", "Wee Care Director"));
 
+        //generates html elements for each staff member and adds them to the document body
         let index: number = 0;
         while (index < staffMemberList.length) {
             document.body.appendChild(staffMemberList[index].getDiv());
