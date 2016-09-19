6d9be80726556ba1ccc6acd8149653d2d11d144d
WesCourtney44 <wescourtney44@gmail.com>
2016-09-19 01:42:30 -0400

    Made the Navigation bar dynamically generated so that each page pulls from the same code. Signed-off-by: WesCourtney44 <wescourtney44@gmail.com>

---
+++ NavigationBar.js
@@ -0,0 +1,33 @@
+//Adds the navigation bar when everything is ready.
+window.onload = function () {
+    NavigationBar.populateNavBar();
+};
+var NavigationBar = (function () {
+    function NavigationBar() {
+    }
+    NavigationBar.populateNavBar = function () {
+        var target = document.getElementById("navigation");
+        target.appendChild(new Link("Home", "Index.html").getLi());
+        target.appendChild(new Link("History", "TaborHistory.html").getLi());
+        target.appendChild(new Link("Calendar", "Calendar.html").getLi());
+        target.appendChild(new Link("Staff", "Staff.html").getLi());
+        target.appendChild(new Link("Wee Care", "WeeCare.html").getLi());
+    };
+    return NavigationBar;
+}());
+var Link = (function () {
+    function Link(text, url) {
+        this.text = text;
+        this.url = url;
+    }
+    Link.prototype.getLi = function () {
+        var li = document.createElement('li');
+        var a = document.createElement('a');
+        a.textContent = this.text;
+        a.href = this.url;
+        li.appendChild(a);
+        return li;
+    };
+    return Link;
+}());
+//# sourceMappingURL=NavigationBar.js.map
---
+++ NavigationBar.ts
@@ -0,0 +1,32 @@
+﻿//Adds the navigation bar when everything is ready.
+window.onload = function () {
+    NavigationBar.populateNavBar();
+};
+
+class NavigationBar {
+    public static populateNavBar(): void {
+        let target: HTMLElement = document.getElementById("navigation");
+        target.appendChild(new Link("Home", "Index.html").getLi());
+        target.appendChild(new Link("History", "TaborHistory.html").getLi());
+        target.appendChild(new Link("Calendar", "Calendar.html").getLi());
+        target.appendChild(new Link("Staff", "Staff.html").getLi());
+        target.appendChild(new Link("Wee Care", "WeeCare.html").getLi());
+    }
+}
+
+class Link {
+    text: string;
+    url: string;
+    constructor(text: string, url: string) {
+        this.text = text;
+        this.url = url;
+    }
+    public getLi(): HTMLLIElement {
+        let li: HTMLLIElement = document.createElement('li');
+        let a: HTMLAnchorElement = document.createElement('a');
+        a.textContent = this.text;
+        a.href = this.url;
+        li.appendChild(a);
+        return li;
+    }
+}
