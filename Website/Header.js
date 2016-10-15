00c88e1139415d43d8eb0f94786f9b9589b042a1
WesCourtney44 <wescourtney44@gmail.com>
2016-10-15 18:05:18 -0400

    Restructured the Project
    Restructured the project so that the web files are internal and don't
    need to be imported by reference.

--- Website/Header.js
+++
@@ -1,9 +0,0 @@
-var Header = (function () {
-    function Header() {
-    }
-    Header.populateHeader = function (target) {
-        target.innerHTML = "<div class=\"clearfix\">< div class=\"logo\" ><a href=\"index.html\" > <img src=\"images/MtTaborChurch.png\" alt= \"LOGO\" height= \"52\" width= \"362\" > </a>< /div>< ul id= \"navigation\" class=\"navigation\" > </ul></div>";
-    };
-    return Header;
-}());
-//# sourceMappingURL=Header.js.map
